62959e2cf2e966408f07b2c6bdf364fba26e906f
OT-P53 <OT-P53@proton.me>
2025-12-29 11:15:13 -0500

    contrato

--- src/app/lib/constants.ts
+++ src/app/lib/constants.ts
@@ -1,3 +1,3 @@
 export const INFURA_GATEWAY: string = "https://thedial.infura-ipfs.io";
 export const SUPERMERCADO: `0x${string}` =
-  "0x54252BD93726433b208FbaAAcA0D884d7C08d4f1";
+  "0x6bDC28003055Df41725dDBE11212104D7B9256c4";
